c209138f60029c81770e2815816925e2171d0b22
rakshithkalmadi <rakshithkalmadi@gmail.com>
2022-07-12 18:12:03 +0530

    init variable

---
+++ script.js
@@ -0,0 +1,8 @@
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
